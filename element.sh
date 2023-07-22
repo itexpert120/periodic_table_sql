59d0da9aaf30cd4acb5f3cdf6a4849c27e74ba46
itexpert120 <itexpert120@gmail.com>
2023-07-22 10:19:16 +0000

    chore: setup initial structue of program

--- element.sh
+++ element.sh
@@ -2,3 +2,17 @@
 
 # Add PSQL command
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# greeting
+MAIN_PROGRAM() {
+  if [[ -z $1 ]]
+  then
+    echo "Please provide an element as an argument."
+  else
+    PRINT_ELEMENT $1
+  fi
+}
+
+PRINT_ELEMENT() {
+  echo "$1"
+}
